2b54162c97ff8011589ed67c40ac099c7bdaa144
Aman Karmani <aman@tmm1.net>
2023-06-20 11:40:58 -0700

    remove tab pinning; fixes #112

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
 chrome.tabs.create(
-	{ pinned: true, active: false, url: `chrome-extension://${chrome.runtime.id}/options.html` },
+	{ active: false, url: `chrome-extension://${chrome.runtime.id}/options.html` },
 	(tab) => {}
 );
