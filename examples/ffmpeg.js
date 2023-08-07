ec5ccad9227d7babb2bbd33f6d482e169f5375a0
SamuelScheit <github@samuelscheit.com>
2023-08-07 19:40:01 +0200

    fix: ffmpeg example

--- examples/ffmpeg.js
+++ examples/ffmpeg.js
@@ -19,18 +19,23 @@ async function test() {
 	const stream = await getStream(page, { audio: true, video: true, frameSize: 1000 });
 	console.log("recording");
 	// this will pipe the stream to ffmpeg and convert the webm to mkv format (which supports vp8/vp9)
-	const ffmpeg = exec(`ffmpeg -y -i - -c copy output.mkv`);
+	const ffmpeg = exec(`ffmpeg -y -i - -c copy output.mp4`);
 	ffmpeg.stderr.on("data", (chunk) => {
 		console.log(chunk.toString());
+	});
+
+	stream.on("close", () => {
+		console.log("stream close");
+		ffmpeg.stdin.end();
 	});
 
 	stream.pipe(ffmpeg.stdin);
 
 	setTimeout(async () => {
 		await stream.destroy();
-		ffmpeg.kill("SIGINT");
 
 		console.log("finished");
+		await browser.close();
 	}, 1000 * 10);
 }
 
